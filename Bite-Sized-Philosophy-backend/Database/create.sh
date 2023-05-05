1b2f26d9ef0d7b93a28cb34f6d9a602f29a1441c
Corey Stamper <cstamper93@gmail.com>
2023-05-05 15:32:17 -0400

    changed baseURL to deployed db

---
+++ Bite-Sized-Philosophy-backend/Database/create.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+export PGPASSWORD='postgres1'
+export PGPORT=15432
+BASEDIR=$(dirname $0)
+DATABASE=philosopher_db
+psql -U postgres -f "$BASEDIR/dropdb.sql" &&
+createdb -U postgres $DATABASE &&
+psql -U postgres -d $DATABASE -f "$BASEDIR/schema.sql"
--- Bite-Sized-Philosophy-backend/Database/philosopher_script.sql
+++ Bite-Sized-Philosophy-backend/Database/philosopher_script.sql
@@ -1,31 +1,30 @@
-rollback;
 
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS philosopher, school, ideas;
+DROP TABLE IF EXISTS philosopher, school, idea;
 
 CREATE TABLE philosopher(
     id SERIAL,
-    philosopher_id CHAR(10) UNIQUE,
     philosopher_name VARCHAR(50),
-    photo VARCHAR(100),
+    photo VARCHAR(300),
     nationality VARCHAR(50),
     era VARCHAR(50),
+    favorited BOOLEAN,
     CONSTRAINT PK_philosopher PRIMARY KEY(id)
 );
 
 CREATE TABLE school(
     school_name VARCHAR(50),
-    philosopher_id CHAR(10),
+    philosopher_id INTEGER,
     CONSTRAINT PK_school PRIMARY KEY(school_name, philosopher_id),
-    CONSTRAINT FK_school_philosopher FOREIGN KEY (philosopher_id) REFERENCES philosopher(philosopher_id)
+    CONSTRAINT FK_school_philosopher FOREIGN KEY (philosopher_id) REFERENCES philosopher(id)
 );
 
 CREATE TABLE idea(
-    quote VARCHAR(500),
-    philosopher_id CHAR(10),
+    quote VARCHAR(2000),
+    philosopher_id INTEGER,
     CONSTRAINT PK_idea PRIMARY KEY(quote, philosopher_id),
-    CONSTRAINT FK_idea_philosopher FOREIGN KEY (philosopher_id) REFERENCES philosopher(philosopher_id)
+    CONSTRAINT FK_idea_philosopher FOREIGN KEY (philosopher_id) REFERENCES philosopher(id)
 );
 
 COMMIT;
